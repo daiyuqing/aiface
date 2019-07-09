8973f6edbe92619e8ac8c4e6f3bae000f7ba291a
daiyuqing <765804005@qq.com>
2019-07-09 23:35:55 +0800

    bug

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ App({
       name: 'login',
       complete: res => {
         self.globalData.openid = res.result.openid;
+        // 获取用户信息
+        wx.getSetting({
+          success: res => {
+            if (res.authSetting['scope.userInfo']) {
+              // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+              wx.getUserInfo({
+                success: res => {
+                  self.addUser(res.userInfo)
+                  self.globalData.userInfo = res.userInfo;
+                }
+              })
+            }
+          }
+        })
       }
     })
     // 登录
@@ -20,20 +34,6 @@ App({
       success: res => {
         console.log(res)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              self.addUser(res.userInfo)
-              self.globalData.userInfo=res.userInfo;
-            }
-          })
-        }
       }
     })
   },
